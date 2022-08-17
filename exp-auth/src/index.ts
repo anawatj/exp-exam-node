832627c67ec17e557624862ec1c005ee75c028b4
anawatj <ajarusiripot@gmail.com>
2022-08-17 11:00:18 +0700

    connect auth db

--- exp-auth/src/index.ts
+++ exp-auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import json from 'body-parser';
+import mongoose from 'mongoose';
 import {currentUserRouter} from './routes/current-user';
 import {signinRouter} from './routes/signin';
 import {signoutRouter} from './routes/signout';
 import {signupRouter} from './routes/signup';
 import {errorHandler} from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 const app = express();
 app.use(json());
 
@@ -12,7 +14,21 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+  });
 app.use(errorHandler);
-app.listen(3000,()=>{
-    console.log("Listening on port 3000!");
-})
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://exp-auth-mongo-srv:27017/auth", {});
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
+};
+
+start();
--- exp-auth/src/routes/signup.ts
+++ exp-auth/src/routes/signup.ts
@@ -2,6 +2,8 @@ import express,{Request,Response} from 'express';
 import {body ,validationResult} from 'express-validator';
 import {RequestValidationError} from '../errors/request-validation-error';
 import {DatabaseConnectionError} from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
+import {User} from '../models/user';
 const router = express.Router();
 router.post("/api/users/signup",
 body('email')
@@ -11,17 +13,21 @@ body('password')
 .trim()
 .isLength({min:4,max:20})
 .withMessage("Password must be between 2 and 20 characters")
-,(req:Request,res:Response)=>{
+,async (req:Request,res:Response)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         throw new RequestValidationError(errors.array());
         
     }
-    const {email,password} = req.body;
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        throw new BadRequestError('Email in use');
+      }
 
-    console.log("Creating user");
-    throw new DatabaseConnectionError();
-    res.status(201).send({})
+    const user = User.build({ email, password });
+    await user.save();
+    res.status(201).send(user)
    
 });
 export {router as signupRouter}
