4faf468e182718018ca8ac308beeab713306b1f6
anawatj <ajarusiripot@gmail.com>
2022-08-17 12:42:27 +0700

    jwt token

--- exp-auth/src/index.ts
+++ exp-auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import json from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from "cookie-session";
 import {currentUserRouter} from './routes/current-user';
 import {signinRouter} from './routes/signin';
 import {signoutRouter} from './routes/signout';
@@ -8,8 +9,14 @@ import {signupRouter} from './routes/signup';
 import {errorHandler} from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
-
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
@@ -19,6 +26,9 @@ app.all('*', async (req, res) => {
   });
 app.use(errorHandler);
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
   try {
     await mongoose.connect("mongodb://exp-auth-mongo-srv:27017/auth", {});
     console.log("Connected to MongoDb");
