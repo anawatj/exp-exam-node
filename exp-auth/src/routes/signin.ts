592053f1940f246bdd262bd402a110d0c6011ce9
anawatj <ajarusiripot@gmail.com>
2022-08-18 08:12:03 +0700

    handle error

--- exp-auth/src/routes/signin.ts
+++ exp-auth/src/routes/signin.ts
@@ -1,11 +1,10 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
 import { Password } from '../services/password';
 import { User } from '../models/user';
-import { validateRequest } from '@taobooks/common';
-import { BadRequestError } from '@taobooks/common';
+import { validateRequest, BadRequestError } from '@taobooks/common';
 
 const router = express.Router();
 
@@ -21,37 +20,42 @@ router.post(
       .withMessage('You must supply a password')
   ],
   validateRequest,
-  async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+  async (req: Request, res: Response,next:NextFunction) => {
+    try{
+      const { email, password } = req.body;
 
-    const existingUser = await User.findOne({ email });
-    if (!existingUser) {
-      throw new BadRequestError('Invalid credentials');
+      const existingUser = await User.findOne({ email });
+      if (!existingUser) {
+        throw new BadRequestError("Invalid credentials");
+      }
+
+      const passwordsMatch = await Password.compare(
+        existingUser.password,
+        password
+      );
+      if (!passwordsMatch) {
+        throw new BadRequestError("Invalid credentials");
+      }
+
+      // Generate JWT
+      const userJwt = jwt.sign(
+        {
+          id: existingUser.id,
+          email: existingUser.email
+        },
+        process.env.JWT_KEY!
+      );
+
+      // Store it on session object
+      req.session = {
+        jwt: userJwt
+      };
+
+      res.status(200).send(existingUser);
+    }catch(err: any){
+        next(err);
     }
-
-    const passwordsMatch = await Password.compare(
-      existingUser.password,
-      password
-    );
-    if (!passwordsMatch) {
-      throw new BadRequestError('Invalid Credentials');
-    }
-
-    // Generate JWT
-    const userJwt = jwt.sign(
-      {
-        id: existingUser.id,
-        email: existingUser.email
-      },
-      process.env.JWT_KEY!
-    );
-
-    // Store it on session object
-    req.session = {
-      jwt: userJwt
-    };
-
-    res.status(200).send(existingUser);
+    
   }
 );
 
---
+++ exp-books/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import json from 'body-parser';
+
+import cookieSession from "cookie-session";
+import {errorHandler,currentUser} from '@taobooks/common';
+import { NotFoundError } from '@taobooks/common';
+import { createBookRouter } from './routes/new';
+import { showBookRouter } from './routes/show';
+import { indexBookRouter } from './routes/index';
+import { updateBookRouter } from './routes/update';
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
+
+app.use(currentUser);
+app.use(createBookRouter);
+app.use(showBookRouter);
+app.use(indexBookRouter);
+app.use(updateBookRouter);
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+export {app};
